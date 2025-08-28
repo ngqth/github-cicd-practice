fc430e243a17f1e78ac1e384e275d27c8774465f
Quoc Thang Nguyen <ngquocthang@gmail.com>
2025-08-28 13:59:58 +1000

    Add logging to add()

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-def add(a, b): 
-    return a + b 
+def add(a, b):
+    print(f"Adding {a} + {b}")
+    return a + b
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     print("Result:", add(2, 3))
