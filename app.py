28058ad9e8175d6bd6921222d3d743cabd263a6e
Quoc Thang Nguyen <ngquocthang@gmail.com>
2025-08-28 14:24:32 +1000

    Add function for multiply

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ def minus(a, b):
     print(f"Subtracting {a} - {b}")
     return a - b
 
+def multiply(a, b):
+    print(f"Multiplying {a} * {b}")
+    return a * b
+
 if __name__ == "__main__":
     print("Result:", add(2, 3))
     print("Result:", minus(5, 2))
+    print("Result:", multiply(3, 4))
