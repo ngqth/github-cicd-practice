9fbc8be90e186b433c3bd0bf8b016675cd25f0b7
Quoc Thang Nguyen <ngquocthang@gmail.com>
2025-08-28 15:20:19 +1000

    Add divide function

--- app.py
+++ app.py
@@ -10,7 +10,12 @@ def multiply(a, b):
     print(f"Multiplying {a} * {b}")
     return a * b
 
+def divide(a, b):
+    print(f"Dividing {a} / {b}")
+    return a / b
+
 if __name__ == "__main__":
     print("Result:", add(2, 3))
     print("Result:", minus(5, 2))
     print("Result:", multiply(3, 4))
+    print("Result:", divide(10, 2))
